8de38498a693aa8d60197b49a34fa1c57d213eaa
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-04 14:56:20 +0700

    create project & add dependencies

---
+++ src/test/java/rifqimuhammadaziz/spring/core/JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
