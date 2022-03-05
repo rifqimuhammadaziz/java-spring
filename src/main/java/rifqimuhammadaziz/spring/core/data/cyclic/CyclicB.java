6878a6bcf52b77dbe44870f6a7d3817444087f80
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-05 09:38:50 +0700

    create Circular Dependency & test

---
+++ src/main/java/rifqimuhammadaziz/spring/core/data/cyclic/CyclicB.java
@@ -0,0 +1,11 @@
+package rifqimuhammadaziz.spring.core.data.cyclic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CyclicB {
+
+    private CyclicC cyclicC;
+}
